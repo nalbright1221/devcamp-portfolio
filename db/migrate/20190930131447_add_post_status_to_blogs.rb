d6c4666afdc9c152fdab1b14a78dca03b2cf2255
Nicole <nalbrigh@kent.edu>
2019-09-30 12:12:51 -0400

    trying to fix migration issues

--- db/migrate/20190930131447_add_post_status_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddPostStatusToBlogs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :blogs, :status, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_131447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.integer "status"       default: 0
+    t.integer "status"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
